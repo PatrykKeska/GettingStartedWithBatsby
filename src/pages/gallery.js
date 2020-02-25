1e02921645265d8fe86e05060a78215c37c8f4e0
Patryk Keska <patrykkeska@Patryks-MacBook-Pro.local>
2020-02-25 08:57:01 +0100

    gallery cms has been added

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,13 +1,74 @@
 import React from "react"
+import { graphql } from 'gatsby';
+import Image from 'gatsby-image'
+import styled from 'styled-components'
+
+export const query = graphql`
+query galleryCms {
+  allDatoCmsGallery {
+    nodes {
+      heading
+      paragraph
+      id
+      modular {
+      photos{
+        fluid(maxWidth:300 maxHeight:400){
+          src
+          ...GatsbyDatoCmsFluid_tracedSVG
+        }
+      }
+      }
+    }
+  }
+}
+`
+const StyledWrapper = styled.article`
+width : 100%; 
+height: 100%;
+`
+
+const GridWrapper = styled.div`
+ width : 100%; 
+ height : 100% ; 
+ display: grid; 
+ grid-template-columns: repeat(1, 1fr); 
+ grid-gap: 20px;
+ padding: 20px 10px;
+
+ @media(min-width:640px){ 
+    grid-template-columns: repeat(2,1fr);
+    padding : 10px; 
+  }
+
+
+  @media(min-width:1001px){ 
+    grid-template-columns: repeat(3,1fr); 
+  }
+
+
+ `
+
+const GalleryPage = ({ data }) => {
+  const { nodes } = data.allDatoCmsGallery;
 
 
 
-const GalleryPage = () => (
-  <>
-    <h1>Hi on Blog Page</h1>
-    <p>Wellcome there .</p>
-  </>
-);
+  return (
+    <>
+      {nodes.map(item => {
+        return (
+          <StyledWrapper key={item.id}>
+            <h1>{item.heading}</h1>
+            <p>{item.paragraph}</p>
+            <GridWrapper>
+
+              {item.modular.map(item => item.photos.map(({ fluid }) => <Image key={fluid.src} fluid={fluid} />))}
+            </GridWrapper>
+          </StyledWrapper>
+        )
+      })}
+    </>
+  )
+};
 
 export default GalleryPage
- 
